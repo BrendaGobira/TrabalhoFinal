2ba67cd03523c120757df5c843adcc3545f786f2
Brenda Gobira <bgobiravalenca@unifei.edu.br>
2023-12-06 14:11:53 -0300

    Adiciona fundos

--- src/pages/Backgrounds/index.jsx
+++ src/pages/Backgrounds/index.jsx
@@ -2,8 +2,10 @@ import BackgroundList from '../../components/BackgroundList';
 import './Backgrounds.css';
 
 const backgrounds = [
-  'https://cdn.awsli.com.br/600x700/175/175683/produto/5294021/8befb43fe5.jpg',
-  'https://static3.tcdn.com.br/img/img_prod/460977/em_breve_the_legend_of_zelda_link_between_worlds_figma_good_smile_company_19343_5_20201211172813.jpg',
+  '/backgrounds/bu-mario.jpeg',
+  '/backgrounds/computer.jpeg',
+  '/backgrounds/flame.jpeg',
+  '/backgrounds/happy-face.jpeg',
 ];
 
 export default function Backgrounds({ favBackgrounds, onFavorite }) {
